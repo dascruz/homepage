f78643fc44cc929beff717eba5b84cf9add407ea
Daniel Sánchez <dscruz.dev@gmail.com>
2023-03-16 15:35:11 +0100

    Add profile picture to assets

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -27,6 +27,8 @@ const Hero = () => {
         </div>
       </div>
 
+      {/* TODO: Add profile picture to hero section, with a purple-blue gradient offsetted outline */}
+
       <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
         <a href="#about">
           <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
