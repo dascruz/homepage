48e7b3386fa4c14bff373241d00effccfa35edcf
Daniel Sánchez <dscruz.dev@gmail.com>
2023-03-16 15:28:00 +0100

    Remove 3D model from hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,6 @@
 import { motion } from "framer-motion";
 
 import { styles } from "../styles";
-import { ComputersCanvas } from "./canvas";
 
 const Hero = () => {
   return (
@@ -17,7 +16,7 @@ const Hero = () => {
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
             <span className="text-[24px] mr-3">Hi, I'm</span>
-            <span class="bg-gradient-to-br from-blue-500 to-violet-500 bg-clip-text text-transparent box-decoration-clone">
+            <span className="bg-gradient-to-br from-blue-500 to-violet-500 bg-clip-text text-transparent box-decoration-clone">
               Daniel
             </span>
           </h1>
@@ -27,8 +26,6 @@ const Hero = () => {
           </p>
         </div>
       </div>
-
-      <ComputersCanvas />
 
       <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
         <a href="#about">
